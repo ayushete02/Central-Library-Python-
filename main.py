19f1562c56140111d7aa545c59c571a3b4ac5ad5
ayushete2005 <75811912+ayushete2005@users.noreply.github.com>
2021-06-20 14:08:47 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,3 @@
-'''This is my first project in Python. (This python project used an Advance python concept this file has no error)
-This project #Central_Library if for Storing books, Borrow books, Returning Books in this library we have one specialization whatever we have borrowed, Return <== data stores in History, if any person login/logout this step also set in history and one more in this project, This project is created like a real machine which can store data for all step attained by the programmer, this project have a sound system that gives attention to programer what happening.
-This project has a file *( RequirePIPinstall )* in this, all the modules are used in this project are named with their updated versions
-Step followed by a User :
-We have to login by our name, it will store login data in another file After login we have to choose some option:
-● Check Available Books
-● Borrow Books
-● Return/Add Books
-● History
-
-In first we can lookout the books present in the library or not. This step shows all that books with their code and the user can use code for borrow books.
-In the second we can borrow books by giving names or codes as input to the program this step deletes the book present in the library.
-In the third User can Add or return books to the library by adding book names as an input in this step books will add to the library as name input by a user after this user can look at their stored books by choosing Checkbooks available.
-In the forth history all the steps covered by the user this data will store in this history user can look there data by selecting this option and if the user wants to delete this data user can delete this, this data is stored in the format of users name, book name, action taken by the user (borrow, return, login/logout) this will store in a formatted document.
-Thanks.
-'''
-        
-#Created by : Ayush Shete #Python project
-
 from LibraryBooks import LibBooks
 from LibraryBooks import Header
 from playsound import playsound
